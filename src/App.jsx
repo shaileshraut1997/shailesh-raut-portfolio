d36f0c245ad1e5c5297643309a6d0f9e67aa7911
Shailesh Raut <shaileshrautofficial@gmail.com>
2026-08-27 20:22:05 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Certifications from "./components/Certifications";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Certifications />
+        <Education />
+        <Contact />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
